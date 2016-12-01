a990b726f98e2e6cd1c7bb7b579e07d8f742466e
Steve Sharp <zimsteve@live.com>
2016-12-01 15:07:22 +1300

    react componets separated and rendering correctly

---
+++ components/balance.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const Balance = (props) =>
+  <h1>This is the balance</h1>
+
+module.exports = Balance
--- index.js
+++ index.js
@@ -1,6 +1,44 @@
 import React from 'react'
 import { render } from 'react-dom'
-import App from './components/app'
+import {createStore} from 'redux'
+import clone from 'clone'
+import Main from './components/main'
+import Login from './components/login'
+import Signup from './components/signup'
+import Balance from './components/balance'
+const reducer = require('./reducer')
 
-render(<App name='bumpbank' />, document.querySelector('main'))
-console.log('welcome to bumpbank')
+var main = document.querySelector('main')
+var app = document.createElement('div')
+main.appendChild(app)
+
+const initialState = {
+  users: [
+    {
+      id: 1,
+      username: 'ron',
+      password: 'ronwins',
+      pin: 1234,
+      accountNumber: '123456-1234567-00',
+      balance: 100.1
+    }],
+  isLoggedin: false
+}
+
+const store = createStore(reducer, initialState)
+
+var main = document.querySelector('main')
+
+store.subscribe( () => {
+  var state = store.getState()
+  render(
+    <div>
+    <Main state={state}/>
+    <Login name="Login" state={state}/>
+    <Signup name="Signup" state={state}/>
+    </div>,
+    main)
+
+})
+
+store.dispatch({type: 'INIT'})
